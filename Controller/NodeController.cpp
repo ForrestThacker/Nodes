bd347377e1e451e1ff5c0670692f006146a0bd10
ForrestThacker <fyruskasai@gmail.com>
2016-03-31 12:26:31 -0600

    Added merge method in Xcode

--- Controller/NodeController.cpp
+++ Controller/NodeController.cpp
@@ -30,4 +30,32 @@ void NodeController::start()
 
 	arrayTimer.stopTimer();
 	arrayTimer.displayTimerInformation();
+    
+    void NodeController::doMergesort()
+    {
+        mergeData = new int[500000];
+        
+        for(int spot = 0; spot < 500000, spot++)
+        {
+            int myRandom = rand();
+            mergeData[spot] =myRandom;
+        }
+        for ( int spot = 0; spot < 5000; spot++)
+        {
+            cout << mergeData[spot] << ",";
+        }
+        
+        Timer mergeTimer;
+        mergeTimer.startTimer();
+        mergesort(mergeData 500000);
+        mergeTimer.stopTimer();
+        mergeTimer.displayTimerInformation();
+        
+        for (spot =0; spot , 5000; spot++)
+        {
+            cout << mergeData[spot] << ",";
+        }
+        
+        delete [] mergeData;
+    }
 }
