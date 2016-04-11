ce50803dbb812dd9b5dd3e7e575c3b26330da0f8
ForrestThacker <fyruskasai@gmail.com>
2016-04-11 11:59:31 -0600

    Added my TreeNode class

---
+++ Model/TreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TreeNode.cpp
+//  Nodes
+//
+//  Created by Thacker, Forrest on 4/11/16.
+//  Copyright © 2016 Thacker, Forrest. All rights reserved.
+//
+
+#include "TreeNode.hpp"
---
+++ Model/TreeNode.hpp
@@ -0,0 +1,14 @@
+//
+//  TreeNode.hpp
+//  Nodes
+//
+//  Created by Thacker, Forrest on 4/11/16.
+//  Copyright © 2016 Thacker, Forrest. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include <stdio.h>
+
+#endif /* TreeNode_hpp */
