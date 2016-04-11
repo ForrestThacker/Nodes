53ddcd74fc5bccc08b4dbcd5b3f5dbb9a5436f21
ForrestThacker <fyruskasai@gmail.com>
2016-04-11 12:12:04 -0600

    Added code to the TreeNode

--- Model/TreeNode.cpp
+++ Model/TreeNode.cpp
@@ -5,5 +5,33 @@
 //  Created by Thacker, Forrest on 4/11/16.
 //  Copyright © 2016 Thacker, Forrest. All rights reserved.
 //
-
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
 #include "TreeNode.hpp"
+
+namespace CTECData
+{
+    template <class Type>
+    class TreeNode : public Node<Type>
+    {
+    private:
+        TreeNode<Type> * leftChild;
+        TreeNode<Type> * rightChild;
+        TreeNode<Type> * parent;
+    
+    public:
+        TreeNode();
+        TreeNode(const Type& value);
+        TreeNode(const Type& value, TreeNode<Type> * parent);
+        
+        void setLeftChild(TreeNode<Type> * leftChild);
+        void setRightChild(TreeNode,Type * rightChild);
+        void setParent(TreeNode<Type>  parent);
+        TreeNode<Type> * getLeftChild();
+        TreeNode<Type> * getRightChild();
+        TreeNode<Type> * getParent();
+        
+    };
+}
+
+#endif /* TreeNode_hpp */
