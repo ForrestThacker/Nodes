3d284faf50a2e9ede6cd7634c315c6699348ad61
ForrestThacker <fyruskasai@gmail.com>
2016-05-05 12:54:43 -0600

    Added size/capacity in cpp.

--- Model/CTECHashTable.cpp
+++ Model/CTECHashTable.cpp
@@ -7,20 +7,19 @@
 //
 
 
+#include "CTECHashTable.hpp"
+#include <iostream>
+
 
 using namespace CTECData;
 using namespace std;
 
-
-#include "CTECHashTable.hpp"
-#include <iostream>
-
 /*
-*
-**/
+*Stes the capacity of the base storage array to 101.
+*/
 
 template <class Type>
-CTECHashTable<Type> :: CTECHashTable()
+CTECHashTable <Type> :: CTECHashTable()
 {
     this->size= 0;
     this->capacity = 101;
@@ -29,16 +28,58 @@ CTECHashTable<Type> :: CTECHashTable()
 }
 
 /*
-**/
+*Deallocates all memeory attached to the internal storage.
+*/
+
 template <class Type>
-CTECHashTable<ype> :: ~CTECHashTable()
+CTECHashTable <Type> :: ~CTECHashTable()
 {
     delete [] internalStorage;
 }
 
+/*
+ *Returns the size of the data structure.
+ */
+
 
 template <clasType>
-int CTECHahTable<Type> :: getSize()
+int CTECHashTable<Type> :: getSize()
 {
     return this->size;
+    
+}
+
+/*
+* Check if the value does not exist in the table. If so
+* it will update the size if needed, then find the place
+* for the item in the table. If that location is filled it will
+* loop over or around until it finds an empty location and update
+* the size of the structure.
+*/
+
+template <class Type>
+void CTECHashTable<Type> :: add(const Type& value)
+{
+    if(!contains(value))
+    {
+        //Resize if needed.
+        if (size/capacity >= this->efficiencyPercentage)
+        {
+            updateSize();
+        }
+        int insertionIndex = findPosition(value);
+        
+        if(internalStorage[insertionIndex] != nullptr)
+        {
+            while (internalStorage[insertionIndex] != nullptr)
+            {
+                insertionIndex = (insertionIndex + 1) %
+                    capacity;
+            }
+        
+        }
+        
+        internalStorge[insertionIndex] = value;
+        size++;
+    }
 }
